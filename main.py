9b901d6cc71d989bbdf20ab75e5544d0d227d812
ccgongi <77668404+ccgongi@users.noreply.github.com>
2023-07-30 02:01:11 -0700

    dd

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"greeting": "Hello, World!", "message": "Dare is the GOAT! jkjk"}
+    return {"greeting": "Hello, World!", "message": "Dare is thgggge GOAT! jkjk"}
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    
